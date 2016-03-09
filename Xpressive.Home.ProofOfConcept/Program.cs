cffa7503a398d3f0a5600079b6586fcee93ba127
Michael Schuler <michael@schuler-hunziker.ch>
2016-03-09 22:58:59 +0100

    Refactoring

--- Xpressive.Home.ProofOfConcept/Program.cs
+++ Xpressive.Home.ProofOfConcept/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
-using Xpressive.Home.ProofOfConcept.Gateways.DateTime;
 using Xpressive.Home.ProofOfConcept.Gateways.Daylight;
+using Xpressive.Home.ProofOfConcept.Gateways.Denon;
 using Xpressive.Home.ProofOfConcept.Gateways.GoogleCalendar;
 using Xpressive.Home.ProofOfConcept.Gateways.MyStrom;
 using Xpressive.Home.ProofOfConcept.Gateways.PhilipsHue;
 using Xpressive.Home.ProofOfConcept.Gateways.Pushalot;
-using Xpressive.Home.ProofOfConcept.Gateways.Sonos;
-using Xpressive.Home.ProofOfConcept.Gateways.TextToSpeech;
 
 namespace Xpressive.Home.ProofOfConcept
 {
@@ -20,66 +16,29 @@ namespace Xpressive.Home.ProofOfConcept
         {
             var random = new Random();
 
-            var subscriptionService = new SubscriptionService();
-            var devicePropertyStore = new DevicePropertyStore();
             var gatewayResolver = new GatewayResolver();
-            new SubscriptionPropertyObserver(subscriptionService, devicePropertyStore, gatewayResolver);
+            var ipAddressService = new IpAddressService();
+            var messageQueue = new MessageQueue();
+            var variableRepository = new VariableRepository(messageQueue);
+            var hueBridgeLocator = new HueBridgeLocator(ipAddressService);
+            var hueAppKeyStore = new HueAppKeyStore();
 
-            gatewayResolver.Register(new Q42PhilipsHueGateway(new HueBridgeLocator(new IpAddressService()), new HueAppKeyStore()));
-            gatewayResolver.Register(new DateTimeGateway());
-            gatewayResolver.Register(new DaylightGateway());
+            gatewayResolver.Register(new Q42PhilipsHueGateway(hueBridgeLocator, hueAppKeyStore, variableRepository, messageQueue));
+            gatewayResolver.Register(new DaylightGateway(variableRepository, messageQueue));
             gatewayResolver.Register(new PushalotGateway());
-            gatewayResolver.Register(new MyStromGateway(new IpAddressService()));
-            gatewayResolver.Register(new TextToSpeechGateway());
+            gatewayResolver.Register(new MyStromGateway(ipAddressService, messageQueue, variableRepository));
             gatewayResolver.Register(new GoogleCalendarGateway());
-            gatewayResolver.Register(new SonosGateway());
-            gatewayResolver.GetAll().ToList().ForEach(g => g.DevicePropertyChanged += (s, e) => devicePropertyStore.Save(e.GatewayName, e.DeviceId, e.Property, e.Value));
+            gatewayResolver.Register(new DenonGateway(ipAddressService));
 
-            int brightness = 60;
-            devicePropertyStore.DevicePropertyChanged += async (s, e) =>
+            var denon = gatewayResolver.Resolve("Denon");
+
+            while (!denon.Devices.Any())
             {
-                if (e.GatewayName.Equals("DateTime", StringComparison.Ordinal) &&
-                    e.Property.Equals("Second") &&
-                    int.Parse(e.Value) % 30 == 0)
-                {
-                    var hueGateway = gatewayResolver.Resolve("Philips Hue");
-                    var spot = hueGateway.Devices.Single(d => d.Name.StartsWith("Test", StringComparison.Ordinal));
+                Thread.Sleep(1000);
+            }
 
-                    //await hueGateway.Set(spot, "Color", $"000000");
-                    //await hueGateway.Set(spot, "Color", $"{random.Next(256).ToString("X2")}0000");
-                    //await hueGateway.Set(spot, "Brightness", brightness.ToString());
-                    brightness--;
-
-                    var deviceAction = new DeviceAction("Philips Hue", spot.Id, "Change Color");
-                    deviceAction.ActionFieldValues.Add("Color", "FF0000");
-                    await hueGateway.Execute(deviceAction);
-                }
-            };
-
-            Thread.Sleep(10000);
-            var sonos = gatewayResolver.Resolve("Sonos");
-            var sonosDevice = sonos.Devices.Single(d => d.Name.Equals("Bad", StringComparison.Ordinal));
-            sonos.Execute(new DeviceAction("Sonos", sonosDevice.Id, "Play"));
-
-            //IDevice ttsDevice;
-            //var ttsFactory = new TextToSpeechGatewayFactory();
-            //ttsFactory.TryCreate(
-            //    gatewayResolver.Resolve("Text to speech"),
-            //    new Dictionary<string, string> { { "Api Key", "" } },
-            //    out ttsDevice);
-
-            //var sayTimeDeviceSubscriptions = new[]
-            //{
-            //    new DeviceSubscription("DateTime", "DateTimeDevice", new Dictionary<string, string>()
-            //    {
-            //        { "Second", "0" },
-            //    }),
-            //};
-            //var sayTimeAction = new DeviceAction("Text to speech", "TTS", "Say Time");
-            //sayTimeAction.ActionFieldValues["Language"] = "de-de";
-            //var sayTimeSubscription = new Subscription("SayTime", sayTimeAction, sayTimeDeviceSubscriptions);
-            //sayTimeSubscription.WaitTime = TimeSpan.FromSeconds(5);
-            //subscriptionService.Add(sayTimeSubscription);
+            var denonDevice = denon.Devices.Single();
+            denon.Execute(new DeviceAction("Denon", denonDevice.Id, "Volume Up"));
 
             Console.ReadLine();
         }
