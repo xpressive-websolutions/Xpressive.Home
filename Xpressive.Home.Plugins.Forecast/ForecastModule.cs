b7f0551f9e37975ac9ad8fc8e64bfe008172332e
Michael Schuler <michael@schuler-hunziker.ch>
2016-08-19 18:38:57 +0200

    Rename forecast module

---
+++ Xpressive.Home.Plugins.Forecast/ForecastModule.cs
@@ -0,0 +1,19 @@
+using Autofac;
+using Xpressive.Home.Contracts.Gateway;
+
+namespace Xpressive.Home.Plugins.Forecast
+{
+    public class ForecastModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ForecastGateway>()
+                .As<IGateway>()
+                .PropertiesAutowired()
+                .SingleInstance()
+                .OnActivated(async h => await h.Instance.StartAsync());
+
+            base.Load(builder);
+        }
+    }
+}
