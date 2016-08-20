9c77411d289578aa6925f1b94e5152a2d9ae0fef
Michael Schuler <michael@schuler-hunziker.ch>
2016-08-20 15:52:04 +0200

    change background depending on season and time

--- Xpressive.Home.WebApi/app/dashboard.js
+++ Xpressive.Home.WebApi/app/dashboard.js
@@ -1,49 +1,79 @@
-﻿(function (jq) {
+﻿(function(jq) {
 
     var xh = angular.module("xpressivehome", ['ngRoute', 'ui.bootstrap']);
 
     // TODO: sidebar mit "pinned" scripts z.B. für alarm scharf stellen, also raumübergreiffend
 
-    xh.controller("dateTimeController", ['$log', '$interval', function ($log, $interval) {
+    xh.controller("dateTimeController", ['$log', '$interval', function($log, $interval) {
         var c = this;
 
-        $interval(function () {
+        $interval(function() {
             c.date = new Date();
         }, 1000);
     }]);
 
-    xh.controller("roomController", ["$log", "$http", function ($log, $http) {
+    xh.controller("backgroundController", ["$interval", function($interval) {
+        var c = this;
+        var seasons = ["spring", "summer", "autumn", "winter"];
+        var times = ["night", "morning", "afternoon", "evening"];
+
+        var calculateClass = function() {
+            var season = "";
+            var time = "";
+            var now = new Date();
+            var month = now.getMonth();
+            var hour = now.getHours();
+
+            if (month < 2) { season = seasons[3]; }
+            else if (month < 5) { season = seasons[0]; }
+            else if (month < 8) { season = seasons[1]; }
+            else if (month < 11) { season = seasons[2]; }
+            else { season = seasons[3]; }
+
+            if (hour < 6) { time = times[0]; }
+            else if (hour < 12) { time = times[1]; }
+            else if (hour < 18) { time = times[2]; }
+            else { time = times[3]; }
+
+            c.selection = season + "-" + time;
+        };
+
+        calculateClass();
+        $interval(calculateClass, 10000);
+    }]);
+
+    xh.controller("roomController", ["$log", "$http", function($log, $http) {
         var c = this;
 
         c.rooms = [];
         c.groups = [];
         c.room = null;
 
-        c.selectRoom = function (room) {
+        c.selectRoom = function(room) {
             c.room = room;
 
-            $http.get("/api/v1/roomscriptgroup?roomId=" + room.id, { cache: false }).then(function (result) {
-                var groups = _.sortBy(result.data, function (g) { return g.sortOrder; });
+            $http.get("/api/v1/roomscriptgroup?roomId=" + room.id, { cache: false }).then(function(result) {
+                var groups = _.sortBy(result.data, function(g) { return g.sortOrder; });
 
-                _.each(groups, function (g) {
+                _.each(groups, function(g) {
                     g.scripts = [];
                 });
 
                 c.groups = groups;
 
-                _.each(c.groups, function (g) {
-                    $http.get("/api/v1/script/group/" + g.id, { cache: false }).then(function (scripts) {
+                _.each(c.groups, function(g) {
+                    $http.get("/api/v1/script/group/" + g.id, { cache: false }).then(function(scripts) {
                         g.scripts = scripts.data;
                     });
                 });
             });
         };
 
-        c.selectScript = function (script) {
+        c.selectScript = function(script) {
             $http.post("/api/v1/script/execute/" + script.id);
         };
 
-        c.toggle = function (caller) {
+        c.toggle = function(caller) {
             var offsets = jq('#' + caller)[0].getBoundingClientRect();
             var dd = jq('#' + caller).find(".dropdown-menu")[0];
             var maxHeight = jq(window).height();
@@ -66,7 +96,7 @@
             dd.style.top = tp;
         };
 
-        $http.get("/api/v1/room", { cache: false }).then(function (rooms) {
+        $http.get("/api/v1/room", { cache: false }).then(function(rooms) {
             c.rooms = _.sortBy(rooms.data, function(r) { return r.sortOrder; });
 
             if (!c.room && c.rooms) {
@@ -75,7 +105,7 @@
         });
     }]);
 
-    xh.controller("weatherController", ["$log", "$http", "$interval", function ($log, $http, $interval) {
+    xh.controller("weatherController", ["$log", "$http", "$interval", function($log, $http, $interval) {
         var c = this;
         var icons = [
             "clear-day",
@@ -112,14 +142,14 @@
         c.variables = [];
 
         var updateWeather = function() {
-            $http.get("/api/v1/gateway/Weather", { cache: false }).then(function (devices) {
+            $http.get("/api/v1/gateway/Weather", { cache: false }).then(function(devices) {
                 if (devices.data) {
                     var device = devices.data[0];
                     c.isEnabled = true;
                     c.forecast[0].name = "Current";
                     c.forecast[0].prefixes.push("H+0.");
 
-                    $http.get("/api/v1/variable/Weather/" + device.id, { cache: false }).then(function (variables) {
+                    $http.get("/api/v1/variable/Weather/" + device.id, { cache: false }).then(function(variables) {
                         var now = new Date();
                         var hour = now.getHours();
                         var hoursUntilNextStep = 6 - (hour % 6);
@@ -141,12 +171,12 @@
                             c.forecast[i + 1].name = labels[(i + index + 1) % 4];
                         }
 
-                        _.each(variables.data, function (v) {
+                        _.each(variables.data, function(v) {
                             dict[v.name] = v.value;
                         });
 
-                        _.each(c.forecast, function (f) {
-                            _.each(f.prefixes, function (p) {
+                        _.each(c.forecast, function(f) {
+                            _.each(f.prefixes, function(p) {
                                 var t = dict[p + "Temperature"];
                                 var c = dict[p + "Icon"];
 
@@ -169,38 +199,40 @@
                             f.tempMin = Math.round(f.tempMin, 0);
                             f.tempMax = Math.round(f.tempMax, 0);
                             f.icon = "wi wi-forecast-io-" + f.icon;
-                            f.prefixes = null;
+                            f.prefixes = [];
                         });
                     });
                 }
+            }, function() {
+                c.isEnabled = false;
             });
         };
 
-        $interval(updateWeather, 1800000); // every 30minutes
-
-        $http.get("/api/v1/gateway", { cache: false }).then(function (gateways) {
-            if (!_.find(gateways.data, function (g) { return g.name === "Weather"; })) {
+        $http.get("/api/v1/gateway", { cache: false }).then(function(gateways) {
+            if (!_.find(gateways.data, function(g) { return g.name === "Weather"; })) {
                 return;
             }
 
             updateWeather();
+
+            $interval(updateWeather, 10 * 60 * 1000); // every 10minutes
         });
     }]);
 
-    xh.controller("musicController", ["$log", "$http", function ($log, $http) {
+    xh.controller("musicController", ["$log", "$http", function($log, $http) {
         var c = this;
 
         c.isEnabled = false;
 
-        $http.get("/api/v1/gateway", { cache: false }).then(function (gateways) {
-            if (!_.find(gateways.data, function (g) { return g === "Sonos"; })) {
+        $http.get("/api/v1/gateway", { cache: false }).then(function(gateways) {
+            if (!_.find(gateways.data, function(g) { return g === "Sonos"; })) {
                 return;
             }
 
             c.isEnabled = true;
             $log.debug("Music is enabled");
 
-            $http.get("/api/v1/gateway/Sonos", { cache: false }).then(function (devices) {
+            $http.get("/api/v1/gateway/Sonos", { cache: false }).then(function(devices) {
                 $log.debug(angular.toJson(devices.data));
             });
         });
