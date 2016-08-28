9635898ec97393392d5de34c3ab718a0cdb2f1b6
Michael Schuler <michael@schuler-hunziker.ch>
2016-08-28 16:09:52 +0200

    add SoftwareUpdateController

---
+++ Xpressive.Home.WebApi/Controllers/SoftwareUpdateController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Xpressive.Home.Contracts.Services;
+
+namespace Xpressive.Home.WebApi.Controllers
+{
+    [RoutePrefix("api/v1/softwareupdate")]
+    public class SoftwareUpdateController : ApiController
+    {
+        private readonly ISoftwareUpdateDownloadService _service;
+
+        public SoftwareUpdateController(ISoftwareUpdateDownloadService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet, Route("hasNewVersion")]
+        public async Task<bool> IsUpdateAvailable()
+        {
+            return await _service.IsNewVersionAvailableAsync();
+        }
+
+        [HttpPost, Route("start")]
+        public async Task Update()
+        {
+            await _service.DownloadNewestVersionAsync();
+        }
+    }
+}
