36b6ae62f677fb0672b9af39121f8a70db6ac002
Michael Schuler <michael@schuler-hunziker.ch>
2017-01-09 18:32:49 +0100

    Add DeviceConfigurationBackupService to speed up device discovery at startup

---
+++ Xpressive.Home.Contracts/Services/IDeviceConfigurationBackupService.cs
@@ -0,0 +1,8 @@
+﻿namespace Xpressive.Home.Contracts.Services
+{
+    public interface IDeviceConfigurationBackupService
+    {
+        void Save<T>(string gatewayName, T deviceConfigurationBackup);
+        T Get<T>(string gatewayName);
+    }
+}
