8cfd7b59c0fdd753c567742aefa4b81232708910
Michael Schuler <michael@schuler-hunziker.ch>
2017-04-06 15:43:38 +0200

    Add tests for Base62 converter

---
+++ Xpressive.Home.Services.Tests/Base62ConvertTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Xpressive.Home.Services.Tests
+{
+    public class Base62ConvertTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Base62ConvertTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Given_a_long_byte_array_then_it_works()
+        {
+            var converter = new Base62Converter();
+            var array = new byte[128];
+            var random = new Random(0);
+            random.NextBytes(array);
+
+            var result = converter.ToBase62(array);
+
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public void Given_a_short_byte_array_then_it_works()
+        {
+            var converter = new Base62Converter();
+            var array = new byte[2];
+            var random = new Random(0);
+            random.NextBytes(array);
+
+            var result = converter.ToBase62(array);
+
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public void Given_a_big_number_then_it_works()
+        {
+            var converter = new Base62Converter();
+
+            var result = converter.ToBase62(UInt64.MaxValue);
+
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public void Given_a_low_number_then_it_works()
+        {
+            var converter = new Base62Converter();
+
+            var result = converter.ToBase62(13);
+
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public void Given_an_empty_array_then_it_doesnt_fail()
+        {
+            var converter = new Base62Converter();
+            var array = new byte[0];
+
+            var result = converter.ToBase62(array);
+
+            _output.WriteLine(result);
+        }
+
+        [Fact]
+        public void Given_zero_then_it_doesnt_fail()
+        {
+            var converter = new Base62Converter();
+
+            var result = converter.ToBase62(0);
+
+            _output.WriteLine(result);
+        }
+    }
+}
---
+++ Xpressive.Home.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Xpressive.Home.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("05db5880-cdd4-4345-803f-b85c7b622f0c")]
