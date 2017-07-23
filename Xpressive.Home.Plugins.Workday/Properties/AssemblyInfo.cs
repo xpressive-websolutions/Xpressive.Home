b1d9f7dfa6df57f9e53793708197a1912bedcc71
Michael Schuler <michael@schuler-hunziker.ch>
2017-07-23 23:29:38 +0200

    Initial commit workday plugin

---
+++ Xpressive.Home.Plugins.Workday/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Xpressive.Home.Plugins.Workday")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("4dfadf7a-b1fb-4092-910e-b9586902bc44")]
