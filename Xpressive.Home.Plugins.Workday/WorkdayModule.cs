a1b9fc163e4bcc4f78091a1921c01e3b2c64cddc
Michael Schuler <michael@schuler-hunziker.ch>
2017-07-24 00:03:35 +0200

    Add worktime plugin gateway, device and calculator

---
+++ Xpressive.Home.Plugins.Workday/WorkdayModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Xpressive.Home.Contracts.Gateway;
+
+namespace Xpressive.Home.Plugins.Workday
+{
+    public class WorkdayModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<WorkdayGateway>()
+                .As<IGateway>()
+                .SingleInstance();
+
+            builder.RegisterType<WorkdayCalculator>().As<IWorkdayCalculator>();
+
+            base.Load(builder);
+        }
+    }
+}
