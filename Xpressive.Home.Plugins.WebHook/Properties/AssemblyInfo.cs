6eee68f892ed306afde5122e86e3d40daa5b43fa
Michael Schuler <michael@schuler-hunziker.ch>
2018-10-09 23:53:19 +0200

    Migrate webhook plugin

--- Xpressive.Home.Plugins.WebHook/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Xpressive.Home.Plugins.WebHook")]
-[assembly: AssemblyDescription("")]
-
-[assembly: Guid("bae935ae-d604-4931-a0b3-0187a6cb9e7e")]
